467af07582e029a40d0702b8ab8f4cbd2d3572e6
Ivan <IvanBychkov@mail.ru>
2021-04-13 17:30:11 +0300

    Инициализируем FileServer обработчик статических файлов

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,3 +1,4 @@
+// https://golangs.org/template-engine
 package main
 
 import (
@@ -10,6 +11,17 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+
+	// Инициализируем FileServer, он будет обрабатывать
+	// HTTP-запросы к статическим файлам из папки "./ui/static".
+	// Обратите внимание, что переданный в функцию http.Dir путь
+	// является относительным корневой папке проекта
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Используем функцию mux.Handle() для регистрации обработчика для
+	// всех запросов, которые начинаются с "/static/". Мы убираем
+	// префикс "/static" перед тем как запрос достигнет http.FileServer
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	log.Println("Запуск сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)
